efe434c74a50f355d62776be0b6ca493b821ced5
dumatf <dumatf@users.noreply.github.com>
2015-05-20 02:56:24 +0000

    sometimes all tests pass, sometimes some tests fail... probably asynchronous execution issue. need to resolve.

--- test/test-server.js
+++ test/test-server.js
@@ -11,9 +11,15 @@ var app = server.app;
 
 chai.use(chaiHttp);
 
+var seedItems;
+
 describe('Shopping List', function() {
   before(function(done) {
     seed.run(function() {
+      // is there a better way to do this ??
+      Item.find(function(err, items) {
+        seedItems = items;
+      });
       done();
     });
   });
@@ -22,5 +28,107 @@ describe('Shopping List', function() {
     Item.remove(function() {
       done();
     });
+  });
+  
+  it('should list items on GET', function(done) {
+    chai.request(app)
+        .get('/items')
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.should.have.length(3);
+          res.body[0].should.be.a('object');
+          res.body[0].should.have.property('name');
+          res.body[0].name.should.be.a('string');
+          res.body[0].name.should.equal('Broad beans');
+          res.body[1].name.should.equal('Tomatoes');
+          res.body[2].name.should.equal('Peppers');
+          done();
+        });
+  });
+  it('should add an item on POST', function(done) {
+    chai.request(app)
+        .post('/items')
+        .send({name: 'Eggs'})
+        .end(function(err, res) {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.name.should.be.a('string');
+          res.body.name.should.equal('Eggs');
+          // is this the simplest way to determine count(*) ??
+          Item.count(function(err, count) {
+            count.should.equal(seedItems.length+1);
+          });
+          done();
+        });
+  });
+  it('should edit an item on PUT', function(done) {
+    chai.request(app)
+        .put('/items/' + seedItems[0]._id)
+        .send({name: 'Bread'})
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.name.should.be.a('string');
+          res.body.name.should.equal('Bread');
+          Item.count(function(err, count) {
+            count.should.equal(seedItems.length);
+          });
+          done();
+        });
+  });
+  it('should delete an item on DELETE', function(done) {
+    chai.request(app)
+        .delete('/items/' + seedItems[2]._id)
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.name.should.be.a('string');
+          res.body.name.should.equal('Peppers');
+          Item.count(function(err, count) {
+            count.should.equal(seedItems.length-1);
+          });
+          done();
+        });
+  });
+  it('should add new item on PUT if id dne', function(done) {
+    chai.request(app)
+        .put('/items/1234abcd1234abcd1234abcd')
+        .send({name: 'Onions'})
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.name.should.be.a('string');
+          res.body.name.should.equal('Onions');
+          Item.count(function(err, count) {
+            count.should.equal(seedItems.length+1);
+          });
+          done();
+        });
+  });
+  it('should return an error on POST with no body', function(done) {
+    chai.request(app)
+        .post('/items')
+        .end(function(err, res) {
+          res.should.have.status(400);
+          done();
+        });
+  });
+  it('should return an error on PUT with no body', function(done) {
+    chai.request(app)
+        .put('/items/0')
+        .end(function(err, res) {
+          res.should.have.status(400);
+          done();
+        });
   });
 });
